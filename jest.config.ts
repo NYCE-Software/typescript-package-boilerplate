98ee4c0b9c1f66d6a8ee8639864be0a5d2c4a74f
Bas Kager <bas@nyce.dev>
2023-12-19 12:05:32 +0100

    Update package dependencies and implement breaking changes

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,24 @@
-import type { Config } from "@jest/types";
-import { pathsToModuleNameMapper } from "ts-jest";
+import { type JestConfigWithTsJest, pathsToModuleNameMapper } from "ts-jest";
 
-export default async (): Promise<Config.InitialOptions> => {
+export default async (): Promise<JestConfigWithTsJest> => {
     const { compilerOptions } = await require("@nyce/config/tsconfig.json");
-    const nyceBaseOptions: Config.InitialOptions = await require("@nyce/config/jest.cjs");
+    // const nyceBaseOptions: Config.InitialOptions = await require("@nyce/config/jest.cjs");
 
     return {
-        ...nyceBaseOptions,
+        preset: "ts-jest/presets/default-esm",
+        transform: {
+            // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+            // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+            "^.+\\.tsx?$": [
+                "ts-jest",
+                {
+                    useESM: true,
+                },
+            ],
+        },
+        testEnvironment: "node",
+        extensionsToTreatAsEsm: [".ts"],
         moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/src/" }) ?? {},
+        modulePathIgnorePatterns: ["<rootDir>/dist/"],
     };
 };
